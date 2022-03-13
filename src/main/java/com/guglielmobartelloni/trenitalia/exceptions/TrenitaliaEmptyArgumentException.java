8315df048bc3299675402128597df5f8098a7797
samoorai <hirotakatech@gmail.com>
2022-03-13 21:27:39 +0100

    Fixed formatting

--- src/main/java/com/guglielmobartelloni/trenitalia/exceptions/TrenitaliaEmptyArgumentException.java
+++ src/main/java/com/guglielmobartelloni/trenitalia/exceptions/TrenitaliaEmptyArgumentException.java
@@ -2,6 +2,6 @@ package com.guglielmobartelloni.trenitalia.exceptions;
 
 public class TrenitaliaEmptyArgumentException extends RuntimeException {
     public TrenitaliaEmptyArgumentException() {
-        super("One or more arugments are empty");
+        super("One or more arugments are empty/null");
     }
 }
--- src/main/java/com/guglielmobartelloni/trenitalia/exceptions/TrenitaliaResponseException.java
+++ src/main/java/com/guglielmobartelloni/trenitalia/exceptions/TrenitaliaResponseException.java
@@ -5,6 +5,7 @@ import kong.unirest.HttpResponse;
 
 public class TrenitaliaResponseException extends RuntimeException {
     public TrenitaliaResponseException(HttpResponse<?> response) {
-        super("Error in the request, response code: " + response.getStatus() + " with message: " + response.getStatusText()+"\nMake sure your parameters are correct");
+        super("Error in the request, response code: " + response.getStatus() + " with message: " + response.getStatusText()
+                +"\nMake sure your parameters are correct");
     }
 }
