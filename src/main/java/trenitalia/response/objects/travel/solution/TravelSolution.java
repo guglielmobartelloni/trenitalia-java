e899499c8bfbb0c5415745fef04465dbbb8218ea
samoorai <hirotakatech@gmail.com>
2022-03-13 15:31:29 +0100

    Added lombok to travelsolution class

--- src/main/java/trenitalia/response/objects/travel/solution/TravelSolution.java
+++ src/main/java/trenitalia/response/objects/travel/solution/TravelSolution.java
@@ -4,7 +4,11 @@ package trenitalia.response.objects.travel.solution;
 import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString
 public class TravelSolution {
 
     @SerializedName("idsolution")
@@ -64,157 +68,5 @@ public class TravelSolution {
     @SerializedName("originalPrice")
     @Expose
     private Double originalPrice;
-
-    public String getIdsolution() {
-        return idsolution;
-    }
-
-    public void setIdsolution(String idsolution) {
-        this.idsolution = idsolution;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getDirection() {
-        return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-
-    public Long getDeparturetime() {
-        return departuretime;
-    }
-
-    public void setDeparturetime(Long departuretime) {
-        this.departuretime = departuretime;
-    }
-
-    public Long getArrivaltime() {
-        return arrivaltime;
-    }
-
-    public void setArrivaltime(Long arrivaltime) {
-        this.arrivaltime = arrivaltime;
-    }
-
-    public Double getMinprice() {
-        return minprice;
-    }
-
-    public void setMinprice(Double minprice) {
-        this.minprice = minprice;
-    }
-
-    public Object getOptionaltext() {
-        return optionaltext;
-    }
-
-    public void setOptionaltext(Object optionaltext) {
-        this.optionaltext = optionaltext;
-    }
-
-    public String getDuration() {
-        return duration;
-    }
-
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
-    public Integer getChangesno() {
-        return changesno;
-    }
-
-    public void setChangesno(Integer changesno) {
-        this.changesno = changesno;
-    }
-
-    public Boolean getBookable() {
-        return bookable;
-    }
-
-    public void setBookable(Boolean bookable) {
-        this.bookable = bookable;
-    }
-
-    public Boolean getSaleable() {
-        return saleable;
-    }
-
-    public void setSaleable(Boolean saleable) {
-        this.saleable = saleable;
-    }
-
-    public List<Train> getTrainlist() {
-        return trainlist;
-    }
-
-    public void setTrainlist(List<Train> trainlist) {
-        this.trainlist = trainlist;
-    }
-
-    public Boolean getOnlycustom() {
-        return onlycustom;
-    }
-
-    public void setOnlycustom(Boolean onlycustom) {
-        this.onlycustom = onlycustom;
-    }
-
-    public List<Object> getExtraInfo() {
-        return extraInfo;
-    }
-
-    public void setExtraInfo(List<Object> extraInfo) {
-        this.extraInfo = extraInfo;
-    }
-
-    public Boolean getShowSeat() {
-        return showSeat;
-    }
-
-    public void setShowSeat(Boolean showSeat) {
-        this.showSeat = showSeat;
-    }
-
-    public Object getSpecialOffer() {
-        return specialOffer;
-    }
-
-    public void setSpecialOffer(Object specialOffer) {
-        this.specialOffer = specialOffer;
-    }
-
-    public List<Object> getTransportMeasureList() {
-        return transportMeasureList;
-    }
-
-    public void setTransportMeasureList(List<Object> transportMeasureList) {
-        this.transportMeasureList = transportMeasureList;
-    }
-
-    public Double getOriginalPrice() {
-        return originalPrice;
-    }
-
-    public void setOriginalPrice(Double originalPrice) {
-        this.originalPrice = originalPrice;
-    }
 
 }
