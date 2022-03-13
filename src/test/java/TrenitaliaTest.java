37ab4579315216875423b730b99c7940e5e4175e
samoorai <hirotakatech@gmail.com>
2022-03-13 15:32:02 +0100

    Added Tests for OneWaytravel

--- src/test/java/TrenitaliaTest.java
+++ src/test/java/TrenitaliaTest.java
@@ -2,7 +2,10 @@ import org.junit.Before;
 import org.junit.Test;
 import trenitalia.Trenitalia;
 import trenitalia.exceptions.TrenitaliaEmptyArgumentException;
+import trenitalia.exceptions.TrenitaliaResponseException;
+import trenitalia.response.objects.travel.solution.TravelSolution;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,5 +46,74 @@ public class TrenitaliaTest {
     public void testAutocompleteStationsWithEmptyArgument() {
         assertThatExceptionOfType(TrenitaliaEmptyArgumentException.class)
                 .isThrownBy(() -> trenitalia.autocompleteStations(""));
+    }
+
+    @Test
+    public void testOneWaySolutionsWithRightParameters() {
+        final String milano = trenitalia.autocompleteStations("milano").get(1);
+        final String roma = trenitalia.autocompleteStations("roma").get(1);
+        final LocalDateTime today = LocalDateTime.now();
+        final List<TravelSolution> travelSolutionList = trenitalia.oneWaySolutions(milano, roma, today, 1, 0, false, false);
+        assertThat(travelSolutionList).isNotEmpty();
+    }
+
+    @Test
+    public void testOneWaySolutionsWithWrongParameters() {
+        final LocalDateTime today = LocalDateTime.now();
+        //Wrong stations (because not autocompleted)
+        assertThatExceptionOfType(TrenitaliaResponseException.class)
+                .isThrownBy(() -> trenitalia
+                        .oneWaySolutions("milano",
+                                "milano",
+                                today,
+                                1,
+                                0,
+                                false,
+                                false))
+                .withMessage("Error in the request, response code: 400 with message: Bad Request\n" +
+                        "Make sure your parameters are correct");
+        assertThatExceptionOfType(TrenitaliaResponseException.class)
+                .isThrownBy(() -> trenitalia
+                        .oneWaySolutions("MILANO AFFORI",
+                                "milano",
+                                today,
+                                1,
+                                0,
+                                false,
+                                false))
+                .withMessage("Error in the request, response code: 400 with message: Bad Request\n" +
+                        "Make sure your parameters are correct");
+
+        //Zero adults and children
+        assertThatExceptionOfType(TrenitaliaResponseException.class)
+                .isThrownBy(() -> trenitalia
+                        .oneWaySolutions("MILANO AFFORI",
+                                "MILANO BOVISA POLITECNICO",
+                                today,
+                                0,
+                                0,
+                                false,
+                                false))
+                .withMessage("Error in the request, response code: 400 with message: Bad Request\n" +
+                        "Make sure your parameters are correct");
+
+        //Wrong date (too old)
+        assertThatExceptionOfType(TrenitaliaResponseException.class)
+                .isThrownBy(() -> trenitalia
+                        .oneWaySolutions("MILANO AFFORI",
+                                "MILANO BOVISA POLITECNICO",
+                                LocalDateTime.of(2018, 3, 20, 10, 0),
+                                1,
+                                0,
+                                false,
+                                false))
+                .withMessage("Error in the request, response code: 500 with message: Internal Server Error\n" +
+                        "Make sure your parameters are correct");
+    }
+
+    @Test
+    public void testOneWayTravelWithEmptyOrNullArguments() {
+        assertThatExceptionOfType(TrenitaliaEmptyArgumentException.class).isThrownBy(() -> trenitalia.oneWaySolutions(null, null, null, 1, 1, false, false));
+        assertThatExceptionOfType(TrenitaliaEmptyArgumentException.class).isThrownBy(() -> trenitalia.oneWaySolutions("", "", LocalDateTime.now(), 1, 1, false, false));
     }
 }
